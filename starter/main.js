b39e613b4188f7a440fa5213c1c340c432bd2657
Temilawanson <105397963+Temilawanson@users.noreply.github.com>
2022-10-13 13:48:34 +0100

    Started hamburger section for smaller screens

---
+++ starter/main.js
@@ -0,0 +1,14 @@
+// ES5
+
+// console.log("send me your account number")
+// var writeup="sapa nice one"
+// console.log(hamburgerdiv)
+// console.log(document)
+// alert(writeup)
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+
+function dosomething() {
+   hamburgerdiv.classList.toggle("showburger")
+}
