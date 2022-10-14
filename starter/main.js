50621a55bed3a1c9e04dcd610a6224c59e3416c1
Temilawanson <105397963+Temilawanson@users.noreply.github.com>
2022-10-14 12:46:00 +0100

    Completed header section for mobile devices

--- starter/main.js
+++ starter/main.js
@@ -7,8 +7,11 @@
 // alert(writeup)
 
 var hamburgerdiv = document.querySelector(".hamburger")
-
+var mobileLinks = document.querySelector(".mobile-links-holder")
+var mybackdrop = document.querySelector(".back-drop")
 
 function dosomething() {
    hamburgerdiv.classList.toggle("showburger")
+   mobileLinks.classList.toggle("show-mobile-links-holder")
+   mybackdrop.classList.toggle("show-backdrop")
 }
